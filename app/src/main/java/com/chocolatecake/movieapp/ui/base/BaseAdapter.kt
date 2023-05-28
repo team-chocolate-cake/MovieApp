b3c0b79dd3e7e372a608b9dd1ceae8ef79a3de41
Saif Alhaider <saifharith90@gmail.com>
2023-05-28 13:31:16 +0300

    add: base adapter

---
+++ app/src/main/java/com/chocolatecake/movieapp/ui/base/BaseAdapter.kt
@@ -0,0 +1,78 @@
+package com.chocolatecake.movieapp.ui.base
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.chocolatecake.movieapp.BR
+
+interface BaseInteractionListener
+
+abstract class BaseAdapter<T>(
+    private var items: List<T>,
+    private val listener: BaseInteractionListener,
+) : RecyclerView.Adapter<BaseAdapter.BaseViewHolder>() {
+
+    @get:LayoutRes
+    abstract val layoutID: Int
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder =
+        ItemViewHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context), layoutID, parent, false
+            )
+        )
+
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+        if (holder is ItemViewHolder) bind(holder, position)
+    }
+
+    open fun bind(holder: ItemViewHolder, position: Int) {
+        holder.binding.apply {
+            setVariable(BR.item, items[position])
+            setVariable(BR.listener, listener)
+        }
+    }
+
+    class ItemViewHolder(val binding: ViewDataBinding) : BaseViewHolder(binding)
+
+    abstract class BaseViewHolder(binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun getItemCount() = items.size
+
+    open  fun setItems(newItems: List<T>) {
+        val diffResult = DiffUtil.calculateDiff(BaseDiffUtil(items, newItems,::areItemsSame, ::areContentSame))
+        items = newItems
+        diffResult.dispatchUpdatesTo(this)
+    }
+
+    open fun areItemsSame(oldItem: T, newItem: T): Boolean {
+        return oldItem?.equals(newItem) == true
+    }
+    open fun areContentSame(oldPosition: T, newPosition: T) = true
+
+    class BaseDiffUtil<T>(
+        private val oldList: List<T>,
+        private val newList: List<T>,
+        private val checkIfSameItem: (oldItem: T, newItem: T) -> Boolean,
+        private val checkIfSameContent: (oldItem: T, newItem: T) -> Boolean
+    ) : DiffUtil.Callback() {
+
+        override fun getOldListSize() = oldList.size
+
+        override fun getNewListSize() = newList.size
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return checkIfSameItem(oldList[oldItemPosition], newList[newItemPosition])
+        }
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return checkIfSameContent(oldList[oldItemPosition], newList[newItemPosition])
+        }
+
+    }
+
+}
