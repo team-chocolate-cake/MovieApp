2b36f55c7fec25a42c792cb0aacad16a754e37d9
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-06-02 01:38:50 +0300

    use update instead of value

--- app/src/main/java/com/chocolatecake/movieapp/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/chocolatecake/movieapp/ui/login/LoginViewModel.kt
@@ -8,6 +8,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -21,42 +22,47 @@ class LoginViewModel @Inject constructor(private val loginUseCase: LoginUseCase)
         val userName = _loginState.value.userName
         val password = _loginState.value.password
 
-        if (userName.isEmpty() || password.isEmpty()) {
-            _loginState.value = _loginState.value.copy(
+        _loginState.update {
+            it.copy(
                 userNameError = if (userName.isEmpty()) "Username is required" else null,
                 passwordError = if (password.isEmpty()) "Password is required" else null
             )
-            return
         }
 
         viewModelScope.launch {
-            _loginState.value = _loginState.value.copy(onLoading = true)
+            _loginState.update { it.copy(onLoading = true) }
 
             val success = loginUseCase(userName, password)
 
-            _loginState.value = if (success) {
-                _loginState.value.copy(
-                    userNameError = null,
-                    passwordError = null,
-                    onLoading = false
-                )
+            if (success) {
+                _loginState.update {
+                    it.copy(
+                        userNameError = null,
+                        passwordError = null,
+                        onLoading = false
+                    )
+                }
+
             } else {
-                _loginState.value.copy(
-                    userName = "",
-                    password = "",
-                    passwordError = "Login failed",
-                    onLoading = false
-                )
+                _loginState.update {
+                    it.copy(
+                        userName = "",
+                        password = "",
+                        passwordError = "Login failed",
+                        onLoading = false
+                    )
+                }
             }
         }
     }
 
     fun onUserNameChanged(userName: String) {
-        _loginState.value = _loginState.value.copy(userName = userName, userNameError = null)
-        Log.d("mimoo","password $userName")
+        _loginState.update { it.copy(userName = userName, userNameError = null) }
+        Log.d("mimoo", "password $userName")
     }
+
     fun onPasswordChanged(password: String) {
-        _loginState.value = _loginState.value.copy(password = password, passwordError = null)
-        Log.d("mimoo","password $password")
+        _loginState.update { it.copy(password = password, passwordError = null) }
+        Log.d("mimoo", "password $password")
     }
 }
