90e4101007edf41e429a778156bb85b527cdb659
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-06-02 17:24:05 +0200

    add splash screen

--- app/src/main/java/com/chocolatecake/movieapp/MainActivity.kt
+++ app/src/main/java/com/chocolatecake/movieapp/MainActivity.kt
@@ -2,11 +2,13 @@ package com.chocolatecake.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
