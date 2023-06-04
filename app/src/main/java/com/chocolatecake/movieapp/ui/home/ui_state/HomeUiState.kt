d554514383088291b0e8e22ec4b74a45869e095a
Banan Kamal <bananhmdan962@gmail.com>
2023-06-04 21:20:41 +0300

    update ui home state

--- app/src/main/java/com/chocolatecake/movieapp/ui/home/ui_state/HomeUiState.kt
+++ app/src/main/java/com/chocolatecake/movieapp/ui/home/ui_state/HomeUiState.kt
@@ -1,13 +1,13 @@
 package com.chocolatecake.movieapp.ui.home.ui_state
 
 data class HomeUiState(
-    val upComingMovies:List<UpComingMoviesUiState>,
-    val nowPlayingMovies:List<NowPlayingUiState>,
-    val trendingMovies:List<TrendingMoviesUiState> ,
-    val popularPeople:List<PopularPeopleUiState>,
-    val popularMovies:List<PopularMoviesUiState>,
-    val topRated:List<TopRatedUiState>,
-    val recommended:List<RecommendedUiState>,
+    val upComingMovies:List<UpComingMoviesUiState> = emptyList(),
+    val nowPlayingMovies:List<NowPlayingUiState> = emptyList(),
+    val trendingMovies:List<TrendingMoviesUiState> = emptyList() ,
+    val popularPeople:List<PopularPeopleUiState> = emptyList(),
+    val popularMovies:List<PopularMoviesUiState> = emptyList(),
+    val topRated:List<TopRatedUiState> = emptyList(),
+    val recommended:List<RecommendedUiState> = emptyList(),
     val onErrors:List<Error> = emptyList(),
     val loading:Boolean=false,
 )
