a45a0c4701fd394b992b9f90fe21f3c95cdf5424
Mahmoud <Elboray>
2023-06-05 14:43:53 +0300

    add inject in home viewModel

--- app/src/main/java/com/chocolatecake/movieapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/chocolatecake/movieapp/ui/home/HomeViewModel.kt
@@ -1,15 +1,38 @@
 package com.chocolatecake.movieapp.home
 
 import com.chocolatecake.movieapp.data.repository.MovieRepository
+import com.chocolatecake.movieapp.domain.usecases.home.GetNowPlayingUseCase
+import com.chocolatecake.movieapp.domain.usecases.home.GetPopularMoviesUseCase
+import com.chocolatecake.movieapp.domain.usecases.home.GetPopularPeopleUsecase
+import com.chocolatecake.movieapp.domain.usecases.home.GetRecommendedUseCase
+import com.chocolatecake.movieapp.domain.usecases.home.GetTopRatedUseCase
+import com.chocolatecake.movieapp.domain.usecases.home.GetTrendingMoviesUseCase
+import com.chocolatecake.movieapp.domain.usecases.home.GetUpcomingMoviesUseCase
 import com.chocolatecake.movieapp.home.adapter.HomeListener
 import com.chocolatecake.movieapp.ui.base.BaseViewModel
+import com.chocolatecake.movieapp.ui.home.ui_state.HomeUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 
-class HomeViewModel @Inject constructor(private val repository: MovieRepository) :
-    BaseViewModel() , HomeListener{
+class HomeViewModel @Inject constructor(
+    val nowPlayingUseCase: GetNowPlayingUseCase,
+    val popularMoviesUseCase: GetPopularMoviesUseCase,
+    val popularPeopleUseCase: GetPopularPeopleUsecase,
+    val recommendedUseCase: GetRecommendedUseCase,
+    val topRatedUseCase: GetTopRatedUseCase,
+    val trendingMoviesUseCase: GetTrendingMoviesUseCase,
+    upcomingMoviesUseCase: GetUpcomingMoviesUseCase
+) :
+    BaseViewModel(), HomeListener {
+
+    private val _uiState = MutableStateFlow(HomeUiState())
+    val uiState: StateFlow<HomeUiState> = _uiState
+
     override fun getData() {
-        TODO("Not yet implemented")
+
+
     }
 
     override fun onClickNowPlaying(itemId: Int) {
