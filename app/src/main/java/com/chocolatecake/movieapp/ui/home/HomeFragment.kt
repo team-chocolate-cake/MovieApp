ec0846f5439e416354ec2938c7fed61783e6708d
Mahmoud <Elboray>
2023-06-05 16:03:48 +0300

    add fragment home

--- app/src/main/java/com/chocolatecake/movieapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/chocolatecake/movieapp/ui/home/HomeFragment.kt
@@ -1,4 +1,22 @@
 package com.chocolatecake.movieapp.home
 
-class HomeFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import com.chocolatecake.movieapp.R
+import com.chocolatecake.movieapp.databinding.FragmentHomeBinding
+import com.chocolatecake.movieapp.home.adapter.HomeAdapter
+import com.chocolatecake.movieapp.ui.base.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>() {
+    override val layoutIdFragment: Int = R.layout.fragment_home
+    override val viewModel: HomeViewModel by viewModels()
+    private lateinit var adapter: HomeAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
