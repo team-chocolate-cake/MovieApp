26b5abf0e81b7bec135afafda17fe552169aa737
mohammed-akkad <momoo362514@gmail.com>
2023-06-13 08:08:40 +0300

    cleanup

--- app/src/main/java/com/chocolatecake/movieapp/ui/search/SearchAdapter.kt
+++ app/src/main/java/com/chocolatecake/movieapp/ui/search/SearchAdapter.kt
@@ -5,5 +5,5 @@ import com.chocolatecake.movieapp.ui.base.BaseAdapter
 
 class SearchAdapter(private val list: List<SearchUiState.MediaUIState>, private val listener: SearchListener) :
     BaseAdapter<SearchUiState.MediaUIState>(list,listener) {
-    override val layoutID: Int = R.layout.search_item_movie
+    override val layoutID: Int = R.layout.item_movie_horizontal
 }
