771748f63bde59a96c9c6e8ed48288180afbaf55
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-06-13 06:50:51 +0300

    trivia with build error too :(

--- app/src/main/java/com/chocolatecake/movieapp/resourses_helper/NavigationResImpl.kt
+++ app/src/main/java/com/chocolatecake/movieapp/resourses_helper/NavigationResImpl.kt
@@ -7,5 +7,5 @@ import javax.inject.Singleton
 
 @Singleton
 class NavigationResImpl @Inject constructor() : NavigationRes {
-    override val homeFeature: Int = com.chocolatecake.ui.R.id.home_nav_graph
+    override val homeFeature: Int = R.id.action_loginFragment_to_home_nav_graph
 }
