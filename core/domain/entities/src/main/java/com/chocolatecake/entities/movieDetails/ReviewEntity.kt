e6e3cc3ca9c82cf6532523fde092e02430abde5c
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-06-15 20:04:05 +0200

    add reviews in domain

---
+++ core/domain/entities/src/main/java/com/chocolatecake/entities/movieDetails/ReviewEntity.kt
@@ -0,0 +1,9 @@
+package com.chocolatecake.entities.movieDetails
+
+
+data class ReviewEntity(
+    val name:String?,
+    val avatar_path:String?,
+    val content:String?,
+    val created_at:String?,
+)
