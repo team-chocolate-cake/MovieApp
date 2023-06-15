593fccea069ffa1beacfd4d31faa10ae9258d741
mohammed-akkad <momoo362514@gmail.com>
2023-06-15 18:51:23 +0300

    add fragment screen

---
+++ feature/trvia/presentation/ui/src/main/java/com/chocolatecake/ui/type_game/TypeGameFragment.kt
@@ -0,0 +1,28 @@
+package com.chocolatecake.ui.type_game
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.chocolatecake.ui.trivia.R
+import com.chocolatecake.ui.trivia.databinding.FragmentTypeGamesBinding
+
+class TypeGameFragment: Fragment() {
+    lateinit var binding: FragmentTypeGamesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_type_games, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
