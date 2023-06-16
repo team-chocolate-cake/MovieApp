41d41f6207856a11457b5a41d0765aa65d3fd4ec
Saif Alhaider <saifharith90@gmail.com>
2023-06-16 19:16:07 +0300

    add:bottom sheet fragment

---
+++ feature/home/presentation/home-ui/src/main/java/com/chocolatecake/ui/tv_details/RateBottomSheet.kt
@@ -0,0 +1,49 @@
+package com.chocolatecake.ui.tv_details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import com.chocolatecake.ui.home.BR
+import com.chocolatecake.ui.home.R
+import com.chocolatecake.ui.home.databinding.TvDetailsItemBotomSheetBinding
+import com.chocolatecake.viewmodel.tv_details.TvDetailsViewModel
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RateBottomSheet : BottomSheetDialogFragment() {
+    private lateinit var binding: TvDetailsItemBotomSheetBinding
+    val viewModel by activityViewModels<TvDetailsViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding =
+            DataBindingUtil.inflate(
+                inflater,
+                R.layout.tv_details_item_botom_sheet,
+                container,
+                false
+            )
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            setVariable(BR.viewModel, viewModel)
+            return root
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.buttonApply.setOnClickListener{
+            viewModel.onRatingSubmit()
+            dismiss()
+        }
+    }
+}
