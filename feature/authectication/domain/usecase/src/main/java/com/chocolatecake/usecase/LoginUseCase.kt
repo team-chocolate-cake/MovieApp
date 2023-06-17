cc678820949a086c4f68d6ccac6c7ef68130dd8a
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-06-17 23:07:56 +0300

    refactor login use case and nav graph

--- feature/authectication/domain/usecase/src/main/java/com/chocolatecake/usecase/LoginUseCase.kt
+++ feature/authectication/domain/usecase/src/main/java/com/chocolatecake/usecase/LoginUseCase.kt
@@ -1,28 +1,31 @@
 package com.chocolatecake.usecase
 
 import com.chocolatecake.repository.AuthRepository
-import com.chocolatecake.usecase.model.LoginInputErrors
-import com.chocolatecake.usecase.model.LoginStateIndicator
 import javax.inject.Inject
 
 class LoginUseCase @Inject constructor(
     private val authRepository: AuthRepository,
     private val getIsValidLoginUseCase: GetIsValidLoginUseCase
 ) {
-    suspend operator fun invoke(username: String, password: String): LoginStateIndicator {
-        return when (getIsValidLoginUseCase(username, password)) {
-            LoginInputErrors.USER_NAME_ERROR -> LoginStateIndicator.USER_NAME_ERROR
-            LoginInputErrors.PASSWORD_ERROR -> LoginStateIndicator.PASSWORD_ERROR
-            LoginInputErrors.NO_INPUT_ERRORS -> tryToLogin(username, password)
+    suspend operator fun invoke(username: String, password: String): LoginError {
+        val inputErrors = getIsValidLoginUseCase(username, password)
+        return if (inputErrors != LoginError.NO_INPUT_ERRORS) {
+            inputErrors
+        } else {
+            try {
+                authRepository.login(username, password)
+                LoginError.SUCCESS
+            } catch (throwable: Throwable) {
+                LoginError.REQUEST_ERROR
+            }
         }
     }
+}
 
-    private suspend fun tryToLogin(username: String, password: String): LoginStateIndicator {
-        return try {
-            authRepository.login(username, password)
-            LoginStateIndicator.SUCCESS
-        } catch (throwable: Throwable) {
-            LoginStateIndicator.REQUEST_ERROR
-        }
-    }
+enum class LoginError {
+    USER_NAME_ERROR,
+    PASSWORD_ERROR,
+    REQUEST_ERROR,
+    NO_INPUT_ERRORS,
+    SUCCESS
 }
