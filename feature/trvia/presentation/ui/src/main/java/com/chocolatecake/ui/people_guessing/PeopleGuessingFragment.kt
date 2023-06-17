5ce62e359e4984a5e4f04a13e7d88151579bd98b
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-06-17 04:03:55 +0300

    handle people game fragment

--- feature/trvia/presentation/ui/src/main/java/com/chocolatecake/ui/people_guessing/PeopleGuessingFragment.kt
+++ feature/trvia/presentation/ui/src/main/java/com/chocolatecake/ui/people_guessing/PeopleGuessingFragment.kt
@@ -1,6 +1,9 @@
 package com.chocolatecake.ui.people_guessing
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.chocolatecake.bases.BaseFragment
 import com.chocolatecake.ui.trivia.R
 import com.chocolatecake.ui.trivia.databinding.FragmentPeopleGuessingBinding
@@ -8,34 +11,47 @@ import com.chocolatecake.viewmodel.common.model.GameUIEvent
 import com.chocolatecake.viewmodel.common.model.GameUiState
 import com.chocolatecake.viewmodel.people_guessing.PeopleGuessingViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
 @AndroidEntryPoint
-class PeopleGuessingFragment: BaseFragment<FragmentPeopleGuessingBinding,GameUiState,GameUIEvent>() {
+class PeopleGuessingFragment :
+    BaseFragment<FragmentPeopleGuessingBinding, GameUiState, GameUIEvent>() {
+
     override val layoutIdFragment: Int = R.layout.fragment_people_guessing
     override val viewModel: PeopleGuessingViewModel by viewModels()
 
-    override fun onEvent(event: GameUIEvent) {
-        when(event){
-            GameUIEvent.NavigateToLoserScreen -> navigateToWelcomeGameScreen()
-            is GameUIEvent.NavigateToWinnerScreen -> navigateToWinnerScreen(event.level,event.points)
-            GameUIEvent.ShowTimeOut -> showTimeOut()
-            is GameUIEvent.UpdateQuestion -> updateQuestion(event.questionNumber)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.includeItemCardChoosePeople.listener = viewModel
+        initData()
+    }
+
+    private fun initData() {
+        collectLatest {
+            viewModel.state.collectLatest { state ->
+                binding.includeItemHeaderGame.item = state
+                binding.includeItemCardChoosePeople.item = state
+            }
         }
     }
 
-    private fun navigateToWelcomeGameScreen() {
-        TODO("Not yet implemented")
-    }
+    override fun onEvent(event: GameUIEvent) {
+        when (event) {
+            GameUIEvent.NavigateToLoserScreen -> {
+                findNavController().navigate(PeopleGuessingFragmentDirections.actionPeopleGuessingFragmentToTypeGameFragment())
+            }
 
-    private fun navigateToWinnerScreen(level: Int, points: Int) {
+            is GameUIEvent.NavigateToWinnerScreen -> {
+                findNavController().navigate(
+                    PeopleGuessingFragmentDirections.actionPeopleGuessingFragmentToCongratsFragment(
+                        event.gameType
+                    )
+                )
+            }
 
-    }
-
-    private fun showTimeOut() {
-        TODO("Not yet implemented")
-    }
-
-    private fun updateQuestion(questionNumber: Int) {
-
+            GameUIEvent.ShowTimeOut -> {
+                showSnackBar(getString(R.string.time_out))
+            }
+        }
     }
 }
