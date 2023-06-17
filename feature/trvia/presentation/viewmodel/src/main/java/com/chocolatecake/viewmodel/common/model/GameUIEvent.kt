33e06034e27f77f168259f42aa594d997d0452a4
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-06-17 04:02:53 +0300

    handle people game navigation

--- feature/trvia/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/common/model/GameUIEvent.kt
+++ feature/trvia/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/common/model/GameUIEvent.kt
@@ -1,7 +1,6 @@
 package com.chocolatecake.viewmodel.common.model
 
 sealed interface GameUIEvent {
-    data class UpdateQuestion(val questionNumber: Int): GameUIEvent
     data class NavigateToWinnerScreen(val gameType: GameType): GameUIEvent
     object NavigateToLoserScreen: GameUIEvent
     object ShowTimeOut : GameUIEvent
