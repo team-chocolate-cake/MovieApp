e7b35aef5f981d93e231a01a7daa5056f713bd71
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-06-20 01:07:27 +0300

    refactor use cases

--- core/data/repository/src/main/java/com/chocolatecake/repository/tv_shows/OnTheAirTVShowsPagingSource.kt
+++ core/data/repository/src/main/java/com/chocolatecake/repository/tv_shows/OnTheAirTVShowsPagingSource.kt
@@ -1,8 +1,6 @@
 package com.chocolatecake.repository.tv_shows
 
-import android.util.Log
 import com.chocolatecake.entities.TVShowsEntity
-import com.chocolatecake.remote.response.dto.TVShowsRemoteDto
 import com.chocolatecake.remote.service.MovieService
 import com.chocolatecake.repository.BasePagingSource
 import com.chocolatecake.repository.mappers.domain.tv.TVShowsDomainMapper
@@ -11,17 +9,11 @@ import javax.inject.Inject
 class OnTheAirTVShowsPagingSource @Inject constructor(
     service: MovieService,
     private val mapper: TVShowsDomainMapper
-) :
-    BasePagingSource<TVShowsEntity>(service) {
+) : BasePagingSource<TVShowsEntity>(service) {
 
     override suspend fun fetchData(page: Int): List<TVShowsEntity> {
 
         val response = service.getOnTheAirTVShows(page).body()?.results?.filterNotNull()
-        val tvShowsEntities = response?.map { mapper.map(it) } ?: emptyList()
-
-        val test = service.getOnTheAirTVShows(page).isSuccessful
-        Log.d("source--OnTheAir", "$test")
-
-        return tvShowsEntities
+        return response?.map { mapper.map(it) } ?: emptyList()
     }
 }
--- core/data/repository/src/main/java/com/chocolatecake/repository/tv_shows/TopRatedTVShowsPagingSource.kt
+++ core/data/repository/src/main/java/com/chocolatecake/repository/tv_shows/TopRatedTVShowsPagingSource.kt
@@ -1,8 +1,6 @@
 package com.chocolatecake.repository.tv_shows
 
-import android.util.Log
 import com.chocolatecake.entities.TVShowsEntity
-import com.chocolatecake.remote.response.dto.TVShowsRemoteDto
 import com.chocolatecake.remote.service.MovieService
 import com.chocolatecake.repository.BasePagingSource
 import com.chocolatecake.repository.mappers.domain.tv.TVShowsDomainMapper
@@ -16,11 +14,6 @@ class TopRatedTVShowsPagingSource @Inject constructor(
     override suspend fun fetchData(page: Int): List<TVShowsEntity> {
 
         val response = service.getTopRatedTVShows(page).body()?.results?.filterNotNull()
-        val tvShowsEntities = response?.map { mapper.map(it) } ?: emptyList()
-
-        val test = service.getAiringTodayTVShows(page).isSuccessful
-        Log.d("source--TopRated", "$test")
-
-        return tvShowsEntities
+        return response?.map { mapper.map(it) } ?: emptyList()
     }
 }
