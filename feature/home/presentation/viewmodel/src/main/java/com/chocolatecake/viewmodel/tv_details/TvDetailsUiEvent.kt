8651c82544688ab148c4475daaff2ee1df2822a2
Saif Alhaider <saifharith90@gmail.com>
2023-06-20 23:16:07 +0300

    refactor: ui state

--- feature/home/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/tv_details/TvDetailsUiEvent.kt
+++ feature/home/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/tv_details/TvDetailsUiEvent.kt
@@ -3,11 +3,11 @@ package com.chocolatecake.viewmodel.tv_details
 sealed interface TvDetailsUiEvent {
     object Rate : TvDetailsUiEvent
     data class PlayButton(val youtubeKey: String) : TvDetailsUiEvent
-    data class ApplyRating(val message:String) : TvDetailsUiEvent
+    data class ApplyRating(val message: String) : TvDetailsUiEvent
     data class OnPersonClick(val id: Int) : TvDetailsUiEvent
     data class OnSeasonClick(val id: Int) : TvDetailsUiEvent
     data class OnRecommended(val id: Int) : TvDetailsUiEvent
-    object Back:TvDetailsUiEvent
-    object OnShowMoreCast:TvDetailsUiEvent
-    object OnShowMoreRecommended:TvDetailsUiEvent
+    object Back : TvDetailsUiEvent
+    object OnShowMoreCast : TvDetailsUiEvent
+    object OnShowMoreRecommended : TvDetailsUiEvent
 }
