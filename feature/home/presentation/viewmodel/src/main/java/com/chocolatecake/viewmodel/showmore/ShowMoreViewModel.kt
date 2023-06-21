636b8fe6de8530cd63fd2cd0a07f8ed98d8761a6
Mahmoud <Elboray>
2023-06-21 03:17:43 +0300

    do refactor and handle viewModel

--- feature/home/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/showmore/ShowMoreViewModel.kt
+++ feature/home/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/showmore/ShowMoreViewModel.kt
@@ -1,5 +1,6 @@
 package com.chocolatecake.viewmodel.showmore
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import androidx.paging.map
@@ -9,6 +10,7 @@ import com.chocolatecake.usecase.showmore.GetMoreTopRatedByTypeUseCase
 import com.chocolatecake.usecase.showmore.GetMoreTrendingByTypeUseCase
 import com.chocolatecake.viewmodel.showmore.mappers.ShowMoreUiMapper
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -21,13 +23,20 @@ class ShowMoreViewModel @Inject constructor(
     private val getShowMoreTopRatedByTypeUseCase: GetMoreTopRatedByTypeUseCase,
     private val getShowMoreTrendingByTypeUseCase: GetMoreTrendingByTypeUseCase,
     private val showMoreUiMapper: ShowMoreUiMapper,
-) : BaseViewModel<ShowAllUiState, ShowMoreUiEvent>(ShowAllUiState()), ShowMoreListener {
+      savedStateHandle: SavedStateHandle,
+) : BaseViewModel<ShowMoreUiState, ShowMoreUiEvent>(ShowMoreUiState()), ShowMoreListener {
+
+    private val showMoreType =
+        savedStateHandle.get<ShowMoreType>("showMoreType") ?: ShowMoreType.POPULAR_MOVIES
 
     init {
+        _state.update { it.copy(isLoading = true) }
         getData()
     }
 
     private fun getData() {
+
+        _state.update{it.copy(showMoreType = showMoreType)}
         when (_state.value.showMoreType) {
             ShowMoreType.POPULAR_MOVIES -> getPopularMoviesShowMore()
             ShowMoreType.TOP_RATED -> getTopRatedShowMore()
@@ -37,8 +46,9 @@ class ShowMoreViewModel @Inject constructor(
     }
 
     private fun getPopularMoviesShowMore() {
-        try {
-            viewModelScope.launch {
+        viewModelScope.launch {
+            try {
+
                 val items = getShowMorePopularMoviesByTypeUseCase().map { pagingData ->
                     pagingData.map { Showmore -> showMoreUiMapper.map(Showmore) }
                 }.cachedIn(viewModelScope)
@@ -50,16 +60,16 @@ class ShowMoreViewModel @Inject constructor(
                         errorList = emptyList()
                     )
                 }
+            } catch (throwable: UnknownHostException) {
+                onError(throwable)
             }
-        } catch (throwable: UnknownHostException) {
-         //   onError(throwable)
         }
-
     }
 
     private fun getTopRatedShowMore() {
-        try {
-            viewModelScope.launch {
+        viewModelScope.launch {
+            try {
+
                 val items = getShowMoreTopRatedByTypeUseCase().map { pagingData ->
                     pagingData.map { Showmore -> showMoreUiMapper.map(Showmore) }
                 }.cachedIn(viewModelScope)
@@ -71,16 +81,16 @@ class ShowMoreViewModel @Inject constructor(
                         errorList = emptyList()
                     )
                 }
+            } catch (throwable: UnknownHostException) {
+                onError(throwable)
             }
-        } catch (throwable: UnknownHostException) {
-          //  onError(throwable)
         }
-
     }
 
     private fun getTrendingShowMore() {
-        try {
-            viewModelScope.launch {
+        viewModelScope.launch {
+            try {
+
                 val items = getShowMoreTrendingByTypeUseCase().map { pagingData ->
                     pagingData.map { Showmore -> showMoreUiMapper.map(Showmore) }
                 }.cachedIn(viewModelScope)
@@ -92,14 +102,31 @@ class ShowMoreViewModel @Inject constructor(
                         errorList = emptyList()
                     )
                 }
-            }
-        } catch (throwable: UnknownHostException) {
-//            onError(throwable)
-        }
 
+            } catch (throwable: UnknownHostException) {
+                onError(throwable)
+            }
+        }
+    }
+
+    private fun onError(throwable: Throwable) {
+        val errorMessage = throwable.message ?: "No network connection"
+        showErrorWithSnackBar(errorMessage)
+        _state.update {
+            it.copy(
+                errorList = listOf(errorMessage),
+                isLoading = false
+            )
+        }
+    }
+    private fun showErrorWithSnackBar(messages: String) {
+        sendEvent(ShowMoreUiEvent.ShowSnackBar(messages))
+    }
+    override fun backNavigate() {
+        sendEvent(ShowMoreUiEvent.BackNavigate)
     }
 
     override fun onClickMedia(mediaId: Int) {
-
+        sendEvent(ShowMoreUiEvent.NavigateToMovieDetails(mediaId))
     }
 }
