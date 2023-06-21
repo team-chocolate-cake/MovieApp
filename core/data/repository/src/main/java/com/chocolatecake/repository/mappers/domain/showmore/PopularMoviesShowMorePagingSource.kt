12faccae5fe5e9650626c537bfeddb840228d031
Mahmoud <Elboray>
2023-06-21 23:47:51 +0300

    add domainGenreMapper and movieDao

--- core/data/repository/src/main/java/com/chocolatecake/repository/mappers/domain/showmore/PopularMoviesShowMorePagingSource.kt
+++ core/data/repository/src/main/java/com/chocolatecake/repository/mappers/domain/showmore/PopularMoviesShowMorePagingSource.kt
@@ -1,22 +1,24 @@
 package com.chocolatecake.repository.showmore
 
-import android.util.Log
 import com.chocolatecake.entities.MovieEntity
+import com.chocolatecake.local.database.MovieDao
 import com.chocolatecake.remote.service.MovieService
 import com.chocolatecake.repository.BasePagingSource
-import com.chocolatecake.repository.mappers.domain.movie.DomainPopularMovieMapper
+import com.chocolatecake.repository.mappers.domain.DomainGenreMapper
 import com.chocolatecake.repository.mappers.domain.movie.DomainPopularMovieMapperShowMore
-import com.chocolatecake.repository.mappers.domain.movie.DomainTrendingMoviesMapper
 import javax.inject.Inject
 
-class PopularMoviesShowMorePagingSource  @Inject constructor(
+class PopularMoviesShowMorePagingSource @Inject constructor(
     service: MovieService,
-    private val mapper: DomainPopularMovieMapperShowMore
-) : BasePagingSource<MovieEntity>(service) {
+    private val mapper: DomainPopularMovieMapperShowMore,
+    private val domainGenreMapper: DomainGenreMapper,
+    private val movieDao: MovieDao,
+
+    ) : BasePagingSource<MovieEntity>(service) {
 
     override suspend fun fetchData(page: Int): List<MovieEntity> {
-
         val response = service.getPopularMovies(page).body()?.results?.filterNotNull()
-        return response?.map { mapper.map(it) } ?: emptyList()
+        val genreMovieMapper = domainGenreMapper.map(movieDao.getGenresMovies())
+        return response?.map { mapper.map(it, genreMovieMapper) } ?: emptyList()
     }
 }
--- core/data/repository/src/main/java/com/chocolatecake/repository/mappers/domain/showmore/TrendingShowMorePagingSource.kt
+++ core/data/repository/src/main/java/com/chocolatecake/repository/mappers/domain/showmore/TrendingShowMorePagingSource.kt
@@ -1,20 +1,22 @@
 package com.chocolatecake.repository.showmore
 
 import com.chocolatecake.entities.MovieEntity
+import com.chocolatecake.local.database.MovieDao
 import com.chocolatecake.remote.service.MovieService
 import com.chocolatecake.repository.BasePagingSource
+import com.chocolatecake.repository.mappers.domain.DomainGenreMapper
 import com.chocolatecake.repository.mappers.domain.movie.DomainTrendingMovieMapperShowMore
 import javax.inject.Inject
 
 class TrendingShowMorePagingSource @Inject constructor(
     service: MovieService,
-    private val mapper: DomainTrendingMovieMapperShowMore
+    private val mapper: DomainTrendingMovieMapperShowMore,
+    private val domainGenreMapper: DomainGenreMapper,
+    private val movieDao: MovieDao,
 ) : BasePagingSource<MovieEntity>(service) {
-
     override suspend fun fetchData(page: Int): List<MovieEntity> {
-
         val response = service.getTrendingMovies(page = page).body()?.results?.filterNotNull()
-
-        return response?.map { mapper.map(it) } ?: emptyList()
+        val genreMovieMapper = domainGenreMapper.map(movieDao.getGenresMovies())
+        return response?.map { mapper.map(it, genreMovieMapper) } ?: emptyList()
     }
 }
