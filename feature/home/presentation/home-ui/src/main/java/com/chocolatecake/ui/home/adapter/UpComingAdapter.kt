36b1a2bd18dfff241f9af81e4f9d2409345d67d6
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-06-21 13:43:08 +0300

    refactor view pager adapter

--- feature/home/presentation/home-ui/src/main/java/com/chocolatecake/ui/home/adapter/UpComingAdapter.kt
+++ feature/home/presentation/home-ui/src/main/java/com/chocolatecake/ui/home/adapter/UpComingAdapter.kt
@@ -15,5 +15,4 @@ class UpComingAdapter(
     upComingList: List<UpComingMoviesUiState>, listener: HomeListener
 ) : BaseAdapter<UpComingMoviesUiState>(upComingList, listener) {
     override val layoutID = R.layout.home_item_image_slider
-
 }
