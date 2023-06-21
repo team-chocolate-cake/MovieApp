5b8c9ac1b27be6272e9da7da26c125a264cab3d2
Mahmoud <Elboray>
2023-06-21 03:16:26 +0300

    add data class ShowSnackBar

--- feature/home/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/showmore/ShowMoreUiEvent.kt
+++ feature/home/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/showmore/ShowMoreUiEvent.kt
@@ -3,5 +3,6 @@ package com.chocolatecake.viewmodel.showmore
 sealed interface ShowMoreUiEvent {
     data class NavigateToMovieDetails(val itemId: Int) : ShowMoreUiEvent
     object BackNavigate : ShowMoreUiEvent
+    data class ShowSnackBar(val messages: String) : ShowMoreUiEvent
 
 }
