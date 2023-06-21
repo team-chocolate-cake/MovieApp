6d4ef90685ed2dbe478d3b13faa9ae3cd40fdfab
Mahmoud <Elboray>
2023-06-21 03:16:02 +0300

    add function

--- feature/home/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/showmore/ShowMoreListener.kt
+++ feature/home/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/showmore/ShowMoreListener.kt
@@ -2,6 +2,7 @@ package com.chocolatecake.viewmodel.showmore
 
 import com.chocolatecake.bases.BaseInteractionListener
 
-interface  ShowMoreListener : BaseInteractionListener {
-        fun onClickMedia(mediaId: Int)
-    }
+interface ShowMoreListener : BaseInteractionListener {
+    fun onClickMedia(mediaId: Int)
+    fun backNavigate()
+}
