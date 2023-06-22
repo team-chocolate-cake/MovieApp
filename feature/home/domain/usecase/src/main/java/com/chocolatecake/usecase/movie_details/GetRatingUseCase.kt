80d20d4f89ce72c718195ff14265d803909df419
Saif Alhaider <saifharith90@gmail.com>
2023-06-23 02:51:43 +0300

    add:get rating use case

--- feature/home/domain/usecase/src/main/java/com/chocolatecake/usecase/movie_details/GetRatingUseCase.kt
+++ feature/home/domain/usecase/src/main/java/com/chocolatecake/usecase/movie_details/GetRatingUseCase.kt
@@ -1,13 +1,13 @@
 package com.chocolatecake.usecase.movie_details
 
-import com.chocolatecake.entities.movieDetails.RatingResponseEntity
+import com.chocolatecake.entities.movieDetails.StatusEntity
 import com.chocolatecake.repository.MovieRepository
 import javax.inject.Inject
 
 class GetRatingUseCase @Inject constructor(
     private val movieRepository: MovieRepository
 ) {
-    suspend operator fun invoke(movieId:Int , rate:Float): RatingResponseEntity {
+    suspend operator fun invoke(movieId:Int , rate:Float): StatusEntity {
         return movieRepository.setMovieRate(movieId , rate)
     }
 }
