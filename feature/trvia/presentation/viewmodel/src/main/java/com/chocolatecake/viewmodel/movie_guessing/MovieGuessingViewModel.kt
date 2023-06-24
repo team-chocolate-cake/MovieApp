aa9184f86e0a07cc24dafdfee5773bac910ac229
Saif Alhaider <saifharith90@gmail.com>
2023-06-25 02:35:43 +0300

    edit:movie & people view models

--- feature/trvia/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/movie_guessing/MovieGuessingViewModel.kt
+++ feature/trvia/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/movie_guessing/MovieGuessingViewModel.kt
@@ -1,183 +1,38 @@
 package com.chocolatecake.viewmodel.movie_guessing
 
-import androidx.lifecycle.viewModelScope
-import com.chocolatecake.bases.BaseViewModel
 import com.chocolatecake.entities.QuestionEntity
-import com.chocolatecake.entities.UserEntity
 import com.chocolatecake.usecase.GetCurrentUserUseCase
 import com.chocolatecake.usecase.game.UpdateUserPointsUseCase
 import com.chocolatecake.usecase.game.levelup.LevelUpMoviesUseCase
 import com.chocolatecake.usecase.game.questions.GetCurrentMovieQuestion
 import com.chocolatecake.usecase.game.questions.UpdateMoviesQuestionCountUseCase
-import com.chocolatecake.viewmodel.common.AnswerListener
-import com.chocolatecake.viewmodel.common.model.GameType
-import com.chocolatecake.viewmodel.common.model.GameUIEvent
+import com.chocolatecake.viewmodel.common.BaseTriviaQuestionGameViewModel
 import com.chocolatecake.viewmodel.common.model.GameUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MovieGuessingViewModel @Inject constructor(
-    private val getCurrentUserUseCase: GetCurrentUserUseCase,
+    override val getCurrentUserUseCase: GetCurrentUserUseCase,
     private val getCurrentMovieQuestion: GetCurrentMovieQuestion,
     private val updateMovieQuestionCountUseCase: UpdateMoviesQuestionCountUseCase,
-    private val updateUserPointsUseCase: UpdateUserPointsUseCase,
+    override val updateUserPointsUseCase: UpdateUserPointsUseCase,
     private val levelUpMovieUseCase: LevelUpMoviesUseCase,
-) : BaseViewModel<GameUiState, GameUIEvent>(GameUiState(gameType = GameType.MOVIE)),
-    AnswerListener {
+) : BaseTriviaQuestionGameViewModel(state = GameUiState()) {
 
     init {
         getData()
     }
 
-    private fun getData() {
-        _state.update { it.copy(isLoading = true) }
-        tryToExecute(
-            getCurrentUserUseCase::invoke,
-            ::onSuccessUser,
-            ::onError
-        )
-        tryToExecute(
-            getCurrentMovieQuestion::invoke,
-            ::onSuccessQuestion,
-            ::onError
-        )
+    override val getQuestion: suspend () -> QuestionEntity
+        get() = getCurrentMovieQuestion::invoke
+
+    override suspend fun levelUpUseCase() {
+        levelUpMovieUseCase()
     }
 
-    private fun onSuccessQuestion(questionEntity: QuestionEntity) {
-        _state.update {
-            it.copy(
-                question = questionEntity.question,
-                answers = questionEntity.choices,
-                correctAnswerPosition = questionEntity.correctAnswerPosition,
-                imageUrl = questionEntity.imageUrl,
-                isError = false,
-                isLoading = false
-            )
-        }
-        initTimer()
-    }
-
-    private fun onSuccessUser(user: UserEntity) {
-        _state.update {
-            it.copy(
-                level = user.moviesGameLevel,
-                points = user.totalPoints,
-                questionCount = user.numMoviesQuestionsPassed + 1,
-                isError = false,
-                isLoading = false
-            )
-        }
-    }
-
-    private fun onError(throwable: Throwable) {
-        _state.update { it.copy(isError = true) }
-        sendEvent(GameUIEvent.ShowSnackbar(throwable.message.toString()))
-    }
-
-    private var timerJob: Job? = null
-    private fun initTimer() {
-        _state.update { it.copy(countDownTimer = 30) }
-        timerJob?.cancel()
-        timerJob = viewModelScope.launch {
-            while (true) {
-                if (_state.value.countDownTimer == 0) {
-                    onTimeout()
-                    timerJob = null
-                    cancel()
-                }
-                delay(1000)
-                _state.update { it.copy(countDownTimer = it.countDownTimer - 1) }
-            }
-        }
-    }
-
-    private fun onTimeout() {
-        viewModelScope.launch {
-            sendEvent(GameUIEvent.ShowTimeOut)
-            delay(300)
-            sendEvent(GameUIEvent.NavigateToLoserScreen)
-        }
-    }
-
-    override fun onClickAnswer(position: Int) {
-        val heartCount = _state.value.heartCount
-        val correctAnswer = _state.value.correctAnswerPosition
-        if (correctAnswer != position) {
-            if (heartCount - 1 == 0) {
-                onEmptyHearts()
-            } else {
-                _state.update { it.copy(heartCount = heartCount - 1) }
-                getData()
-            }
-            return
-        }
-        if (_state.value.isLastQuestion) {
-            onUserWins()
-            return
-        }
-        updateToNextQuestion()
-    }
-
-    private fun onUserWins() {
-        viewModelScope.launch(Dispatchers.IO) {
-            kotlin.runCatching {
-                levelUpMovieUseCase()
-                sendEvent(GameUIEvent.NavigateToWinnerScreen(GameType.MOVIE))
-            }.onFailure(::onError)
-        }
-    }
-
-    private fun onEmptyHearts() {
-        state.value.apply {
-            when {
-                (level == 1 && points < 30) ||
-                        (level == 2 && points < 60) ||
-                        (level == 3 && points < 150) -> {
-                    sendEvent(GameUIEvent.NavigateToLoserScreen)
-                }
-
-                else -> {
-                    val numberOfPoints = when (level) {
-                        1 -> 30
-                        2 -> 60
-                        3 -> 150
-                        else -> 0
-                    }
-                    sendEvent(GameUIEvent.ShowBuyHeartDialog(numberOfPoints))
-                    timerJob?.cancel()
-                }
-            }
-        }
-    }
-
-    private fun updateToNextQuestion() {
-        viewModelScope.launch(Dispatchers.IO) {
-            kotlin.runCatching {
-                updateMovieQuestionCountUseCase(_state.value.questionCount)
-                _state.update { it.copy(points = it.points + (it.level * 10)) }
-                updateUserPointsUseCase(_state.value.points)
-                getData()
-            }.onFailure(::onError)
-        }
-    }
-
-    fun retry() {
-        getData()
-        _state.update { it.copy(heartCount = 3) }
-    }
-
-    fun buyHearts(numberOfPoints:Int) {
-        _state.update { it.copy(points = it.points - numberOfPoints) }
-        _state.update { it.copy(heartCount = 3) }
-        initTimer()
-
+    override suspend fun updateQuestionCountUseCase(questionCount: Int) {
+        updateMovieQuestionCountUseCase(questionCount)
     }
 
 }
--- feature/trvia/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/people_guessing/PeopleGuessingViewModel.kt
+++ feature/trvia/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/people_guessing/PeopleGuessingViewModel.kt
@@ -1,156 +1,37 @@
 package com.chocolatecake.viewmodel.people_guessing
 
-import androidx.lifecycle.viewModelScope
-import com.chocolatecake.bases.BaseViewModel
 import com.chocolatecake.entities.QuestionEntity
-import com.chocolatecake.entities.UserEntity
 import com.chocolatecake.usecase.GetCurrentUserUseCase
 import com.chocolatecake.usecase.game.UpdateUserPointsUseCase
 import com.chocolatecake.usecase.game.levelup.LevelUpPeopleUseCase
 import com.chocolatecake.usecase.game.questions.GetCurrentPeopleQuestion
 import com.chocolatecake.usecase.game.questions.UpdatePeopleQuestionCountUseCase
-import com.chocolatecake.viewmodel.common.AnswerListener
-import com.chocolatecake.viewmodel.common.model.GameType
-import com.chocolatecake.viewmodel.common.model.GameUIEvent
+import com.chocolatecake.viewmodel.common.BaseTriviaQuestionGameViewModel
 import com.chocolatecake.viewmodel.common.model.GameUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class PeopleGuessingViewModel @Inject constructor(
-    private val getCurrentUserUseCase: GetCurrentUserUseCase,
+    override val getCurrentUserUseCase: GetCurrentUserUseCase,
     private val getCurrentPeopleQuestion: GetCurrentPeopleQuestion,
     private val updatePeopleQuestionCountUseCase: UpdatePeopleQuestionCountUseCase,
-    private val updateUserPointsUseCase: UpdateUserPointsUseCase,
+    override val updateUserPointsUseCase: UpdateUserPointsUseCase,
     private val levelUpPeopleUseCase: LevelUpPeopleUseCase,
-) : BaseViewModel<GameUiState, GameUIEvent>(GameUiState()), AnswerListener {
+) : BaseTriviaQuestionGameViewModel(state = GameUiState()) {
 
     init {
         getData()
     }
 
-    private fun getData() {
-        _state.update { it.copy(isLoading = true) }
-        tryToExecute(
-            getCurrentUserUseCase::invoke,
-            ::onSuccessUser,
-            ::onError
-        )
-        tryToExecute(
-            getCurrentPeopleQuestion::invoke,
-            ::onSuccessQuestion,
-            ::onError
-        )
+    override val getQuestion: suspend () -> QuestionEntity
+        get() = getCurrentPeopleQuestion::invoke
+
+    override suspend fun levelUpUseCase() {
+        levelUpPeopleUseCase()
     }
 
-    private fun onSuccessQuestion(questionEntity: QuestionEntity) {
-        _state.update {
-            it.copy(
-                question = questionEntity.question,
-                answers = questionEntity.choices,
-                correctAnswerPosition = questionEntity.correctAnswerPosition,
-                imageUrl = questionEntity.imageUrl,
-                isError = false,
-                isLoading = false
-            )
-        }
-        initTimer()
-    }
-
-    private fun onSuccessUser(user: UserEntity) {
-        _state.update {
-            it.copy(
-                level = user.peopleGameLevel,
-                points = user.totalPoints,
-                questionCount = user.numPeopleQuestionsPassed + 1,
-                isError = false,
-                isLoading = false
-            )
-        }
-    }
-
-    private fun onError(throwable: Throwable) {
-        _state.update { it.copy(isError = true) }
-        sendEvent(GameUIEvent.ShowSnackbar(throwable.message.toString()))
-    }
-
-    private var timerJob: Job? = null
-    private fun initTimer() {
-        _state.update { it.copy(countDownTimer = 30) }
-        timerJob?.cancel()
-        timerJob = viewModelScope.launch {
-            while (true) {
-                if (_state.value.countDownTimer == 0) {
-                    onTimeout()
-                    timerJob = null
-                    cancel()
-                }
-                delay(1000)
-                _state.update { it.copy(countDownTimer = it.countDownTimer - 1) }
-            }
-        }
-    }
-
-    private fun onTimeout() {
-        viewModelScope.launch {
-            sendEvent(GameUIEvent.ShowTimeOut)
-            delay(1000)
-            sendEvent(GameUIEvent.NavigateToLoserScreen)
-        }
-    }
-
-    private fun onUserLose() {
-        viewModelScope.launch {
-            sendEvent(GameUIEvent.NavigateToLoserScreen)
-        }
-    }
-
-    override fun onClickAnswer(position: Int) {
-        val heartCount = _state.value.heartCount
-        val correctAnswer = _state.value.correctAnswerPosition
-        if (correctAnswer != position) {
-            if (heartCount - 1 == 0) {
-                onUserLose()
-            } else {
-                _state.update { it.copy(heartCount = heartCount - 1) }
-                updateCurrentQuestion()
-            }
-            return
-        }
-        if (_state.value.isLastQuestion) {
-            onUserWins()
-            return
-        }
-        updateToNextQuestion()
-    }
-
-    private fun onUserWins() {
-        viewModelScope.launch(Dispatchers.IO) {
-            kotlin.runCatching {
-                levelUpPeopleUseCase()
-                sendEvent(GameUIEvent.NavigateToWinnerScreen(GameType.PEOPLE))
-            }.onFailure(::onError)
-        }
-    }
-
-    private fun updateCurrentQuestion() {
-        getData()
-    }
-
-    private fun updateToNextQuestion() {
-        viewModelScope.launch(Dispatchers.IO) {
-            kotlin.runCatching {
-                updatePeopleQuestionCountUseCase(_state.value.questionCount)
-                _state.update { it.copy(points = it.points + (it.level * 10)) }
-                updateUserPointsUseCase(_state.value.points)
-                getData()
-            }.onFailure(::onError)
-        }
+    override suspend fun updateQuestionCountUseCase(questionCount: Int) {
+        updatePeopleQuestionCountUseCase(questionCount)
     }
 }
