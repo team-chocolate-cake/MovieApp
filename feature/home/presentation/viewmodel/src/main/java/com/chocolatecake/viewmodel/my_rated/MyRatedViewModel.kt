c7fb2bdf9b79a28548f37ccdeb172d2dd4a8ec8f
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-06-24 19:21:20 +0200

    prepare functions in viewmodel

--- feature/home/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/my_rated/MyRatedViewModel.kt
+++ feature/home/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/my_rated/MyRatedViewModel.kt
@@ -1,14 +1,74 @@
 package com.chocolatecake.viewmodel.my_rated
 
+import android.util.Log
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import androidx.paging.map
 import com.chocolatecake.bases.BaseViewModel
 import com.chocolatecake.usecase.my_rated.GetMyRatedMoviesUseCase
 import com.chocolatecake.usecase.my_rated.GetMyRatedTVShowsUseCase
+import com.chocolatecake.viewmodel.my_rated.mappers.MyRatedMovieToMovieHorizontalUiMapper
+import com.chocolatecake.viewmodel.my_rated.mappers.MyRatedTvShowToMovieHorizontalUiMapper
+import com.chocolatecake.viewmodel.tv_shows.TVShowsType
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MyRatedViewModel @Inject constructor(
     private val getRatedTVShowsUseCase: GetMyRatedTVShowsUseCase,
     private val getRatedMoviesUseCase: GetMyRatedMoviesUseCase,
+    private val myRatedMovieToMovieHorizontalUiMapper: MyRatedMovieToMovieHorizontalUiMapper,
+    private val myRatedTvShowToMovieHorizontalUiMapper: MyRatedTvShowToMovieHorizontalUiMapper,
 ) : BaseViewModel<MyRatedUiState, MyRatedEvents>(MyRatedUiState()) {
 
 
+    fun getMyRatedMovies(){
+        viewModelScope.launch {
+            try {
+                val items = getRatedMoviesUseCase().map { pagingData ->
+                    pagingData.map { movie -> myRatedMovieToMovieHorizontalUiMapper.map(movie) }
+                }.cachedIn(viewModelScope)
+                _state.update {
+                    it.copy(
+                        MyRateType = MyRateType.Movies,
+                        MyRatedMedia = items,
+                        isLoading = false,
+                        errorList = emptyList()
+                    )
+                }
+            } catch (throwable: Throwable) {
+                onError(throwable)
+            }
+        }
+    }
+    fun getMyRatedTvShow(){
+        viewModelScope.launch {
+            try {
+                val items = getRatedTVShowsUseCase().map { pagingData ->
+                    pagingData.map { tvShow -> myRatedTvShowToMovieHorizontalUiMapper.map(tvShow) }
+                }.cachedIn(viewModelScope)
+                _state.update {
+                    it.copy(
+                        MyRateType = MyRateType.Movies,
+                        MyRatedMedia = items,
+                        isLoading = false,
+                        errorList = emptyList()
+                    )
+                }
+            } catch (throwable: Throwable) {
+                onError(throwable)
+            }
+        }
+    }
+
+    private fun onError(throwable: Throwable) {
+        val errorMessage = throwable.message ?: "No network connection"
+        _state.update {
+            it.copy(
+                errorList = listOf(errorMessage),
+                isLoading = false
+            )
+        }
+    }
 }
---
+++ feature/home/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/my_rated/mappers/MyRatedTvShowToMovieHorizontalUiMapper.kt
@@ -0,0 +1,31 @@
+package com.chocolatecake.viewmodel.my_rated.mappers
+
+import com.chocolatecake.entities.GenreEntity
+import com.chocolatecake.entities.MovieEntity
+import com.chocolatecake.entities.my_rated.MyRatedMovieEntity
+import com.chocolatecake.entities.my_rated.MyRatedTvShowEntity
+import com.chocolatecake.mapper.Mapper
+import com.chocolatecake.viewmodel.common.model.MovieHorizontalUIState
+import javax.inject.Inject
+
+class MyRatedTvShowToMovieHorizontalUiMapper @Inject constructor()  : Mapper<MyRatedTvShowEntity, MovieHorizontalUIState> {
+    override fun map(input: MyRatedTvShowEntity): MovieHorizontalUIState {
+        return MovieHorizontalUIState(
+            id = input.id,
+            rate = input.rate,
+            title = input.title,
+            imageUrl = input.imageUrl,
+            year = extractYearFromDate(input.year),
+            genres = convertGenreListToString(input.genreEntities),
+        )
+    }
+
+    private fun convertGenreListToString(list: List<Int>): String {
+        return list.joinToString(" | ")
+    }
+
+    private fun extractYearFromDate(year: String): String {
+        val parts = year.split("-")
+        return parts[0]
+    }
+}
