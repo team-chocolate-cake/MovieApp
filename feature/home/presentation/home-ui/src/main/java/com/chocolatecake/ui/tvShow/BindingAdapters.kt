d6dc1e13119bc79293aa9850520ec30409675b42
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-06-24 17:54:10 +0300

    add fab

--- feature/home/presentation/home-ui/src/main/java/com/chocolatecake/ui/tvShow/BindingAdapters.kt
+++ feature/home/presentation/home-ui/src/main/java/com/chocolatecake/ui/tvShow/BindingAdapters.kt
@@ -2,9 +2,11 @@ package com.chocolatecake.ui.tvShow
 
 import android.view.View
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.chocolatecake.ui.home.R
 import com.chocolatecake.viewmodel.tv_shows.TVShowsType
 import com.google.android.material.chip.ChipGroup
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 @BindingAdapter(value = ["app:selectedTVShowType"])
 fun ChipGroup.setSelectedTVShowsChip(type: TVShowsType) {
@@ -14,4 +16,25 @@ fun ChipGroup.setSelectedTVShowsChip(type: TVShowsType) {
         TVShowsType.TOP_RATED -> check(R.id.chip_top_rated)
         TVShowsType.POPULAR -> check(R.id.chip_popular)
     }
+}
+
+@BindingAdapter("scrollToTop")
+fun setScrollToTopListener(fab: FloatingActionButton, recyclerView: RecyclerView) {
+    fab.setOnClickListener {
+        recyclerView.smoothScrollToPosition(0)
+    }
+}
+
+@BindingAdapter("showOnScroll")
+fun setShowOnScrollListener(fab: FloatingActionButton, recyclerView: RecyclerView) {
+    recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+            if (dy > 0 && fab.visibility == View.GONE) {
+                fab.show()
+            } else if (dy < 0 && fab.visibility == View.VISIBLE) {
+                fab.hide()
+            }
+        }
+    })
 }
