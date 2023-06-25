64f6f8e7eb6362b68d3f1b36148e58368b1a0e4c
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-06-25 18:56:55 +0300

    create trailer fragment

---
+++ feature/home/presentation/home-ui/src/main/java/com/chocolatecake/ui/youtube_player/TrailerFragment.kt
@@ -0,0 +1,10 @@
+package com.chocolatecake.ui.youtube_player
+
+import com.chocolatecake.bases.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class TrailerFragment:BaseFragment<> {
+
+}
