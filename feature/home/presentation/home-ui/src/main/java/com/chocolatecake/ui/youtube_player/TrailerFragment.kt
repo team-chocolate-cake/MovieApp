73cf3219766f8dc616185128b5770dc7392b94bd
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-06-25 19:01:30 +0300

    create fragment trailer layout

--- feature/home/presentation/home-ui/src/main/java/com/chocolatecake/ui/youtube_player/TrailerFragment.kt
+++ feature/home/presentation/home-ui/src/main/java/com/chocolatecake/ui/youtube_player/TrailerFragment.kt
@@ -1,10 +1,24 @@
 package com.chocolatecake.ui.youtube_player
 
+import androidx.fragment.app.viewModels
 import com.chocolatecake.bases.BaseFragment
+import com.chocolatecake.bases.BaseViewModel
+import com.chocolatecake.ui.home.R
+import com.chocolatecake.ui.home.databinding.FragmentTrailerBinding
+import com.chocolatecake.viewmodel.youtube_trailer.TrailerInteraction
+import com.chocolatecake.viewmodel.youtube_trailer.TrailerViewModel
+import com.chocolatecake.viewmodel.youtube_trailer.YoutubePlayerUIState
 import dagger.hilt.android.AndroidEntryPoint
 
-
 @AndroidEntryPoint
-class TrailerFragment:BaseFragment<> {
+class TrailerFragment :
+    BaseFragment<FragmentTrailerBinding, YoutubePlayerUIState, TrailerInteraction>() {
+
+    override val layoutIdFragment = R.layout.fragment_trailer
+    override val viewModel: TrailerViewModel by viewModels()
+
+    override fun onEvent(event: TrailerInteraction) {
+
+    }
 
 }
