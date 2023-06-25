1fb2812423e829b90ed5b378e211c58532f3b55f
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-06-25 16:52:33 +0200

    fix issue

--- core/data/repository/src/main/java/com/chocolatecake/repository/my_rated/RatedMoviesPagingSource.kt
+++ core/data/repository/src/main/java/com/chocolatecake/repository/my_rated/RatedMoviesPagingSource.kt
@@ -1,18 +1,45 @@
 package com.chocolatecake.repository.my_rated
 
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
 import com.chocolatecake.entities.my_rated.MyRatedMovieEntity
+import com.chocolatecake.remote.response.dto.my_rated.MyRatedMoviesResponseDto
 import com.chocolatecake.remote.service.MovieService
-import com.chocolatecake.repository.BasePagingSource
 import com.chocolatecake.repository.mappers.domain.my_rated.DomainMyRatedMoviesMapper
+import java.io.IOException
 import javax.inject.Inject
 
 
 class RatedMoviesPagingSource @Inject constructor(
-    service: MovieService,
+    private val service: MovieService,
     private val mapper: DomainMyRatedMoviesMapper
-) : BasePagingSource<MyRatedMovieEntity>(service) {
-    override suspend fun fetchData(page: Int): List<MyRatedMovieEntity> {
-        val response = service.getRatedMovies(page).body()?.movies?.filterNotNull()
-        return response?.map { mapper.map(it) } ?: emptyList()
+) : PagingSource<Int, MyRatedMovieEntity>() {
+    suspend fun fetchData(page: Int): MyRatedMoviesResponseDto? {
+        return service.getRatedMovies(page).body()
     }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, MyRatedMovieEntity> {
+        val currentPage = params.key ?: 1
+        val response = fetchData(currentPage)
+        val myRatedMovieDtos = response?.movies?.filterNotNull() ?: emptyList()
+        val nextKey = if (response?.totalPages!! <= currentPage) {
+            currentPage + 1
+        } else {
+            null
+        }
+        return try {
+            LoadResult.Page(
+                data = myRatedMovieDtos.map { mapper.map(it) },
+                prevKey = if (currentPage == 1) null else currentPage - 1,
+                nextKey = nextKey
+            )
+        } catch (e: IOException) {
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, MyRatedMovieEntity>): Int? {
+        return null
+    }
+
 }
--- core/data/repository/src/main/java/com/chocolatecake/repository/my_rated/RatedTvShowPagingSource.kt
+++ core/data/repository/src/main/java/com/chocolatecake/repository/my_rated/RatedTvShowPagingSource.kt
@@ -1,20 +1,44 @@
 package com.chocolatecake.repository.my_rated
 
-import com.chocolatecake.entities.my_rated.MyRatedMovieEntity
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
 import com.chocolatecake.entities.my_rated.MyRatedTvShowEntity
+import com.chocolatecake.remote.response.dto.my_rated.MyRatedTvShowResponseDto
 import com.chocolatecake.remote.service.MovieService
-import com.chocolatecake.repository.BasePagingSource
-import com.chocolatecake.repository.mappers.domain.my_rated.DomainMyRatedMoviesMapper
 import com.chocolatecake.repository.mappers.domain.my_rated.DomainMyRatedTvShowMapper
+import java.io.IOException
 import javax.inject.Inject
 
 
 class RatedTvShowPagingSource @Inject constructor(
-    service: MovieService,
+    private val service: MovieService,
     private val mapper: DomainMyRatedTvShowMapper
-) : BasePagingSource<MyRatedTvShowEntity>(service) {
-    override suspend fun fetchData(page: Int): List<MyRatedTvShowEntity> {
-        val response = service.getRatedTv(page).body()?.tvShows?.filterNotNull()
-        return response?.map { mapper.map(it) } ?: emptyList()
+) : PagingSource<Int, MyRatedTvShowEntity>(){
+     suspend fun fetchData(page: Int): MyRatedTvShowResponseDto? {
+        return service.getRatedTv(page).body()
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, MyRatedTvShowEntity> {
+        val currentPage = params.key ?: 1
+        val response = fetchData(currentPage)
+        val myRatedTvShowsDtos = response?.tvShows?.filterNotNull() ?: emptyList()
+        val nextKey = if (response?.totalPages!! <= currentPage && response.totalPages!=1) {
+            currentPage + 1
+        } else {
+            null
+        }
+        return try {
+            LoadResult.Page(
+                data = myRatedTvShowsDtos.map { mapper.map(it) },
+                prevKey = if (currentPage == 1) null else currentPage - 1,
+                nextKey = nextKey
+            )
+        } catch (e: IOException) {
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, MyRatedTvShowEntity>): Int? {
+        return null
     }
 }
