f1b3b4915c32fef8312d4885cac28db3a0d15be5
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-06-26 19:34:41 +0300

    fix: memorize logic

--- feature/trvia/domain/entities/src/main/java/com/chocolatecake/entities/BoardEntity.kt
+++ feature/trvia/domain/entities/src/main/java/com/chocolatecake/entities/BoardEntity.kt
@@ -2,7 +2,6 @@ package com.chocolatecake.entities
 
 data class BoardEntity(
     val itemsEntity: List<ItemBoardEntity>,
-    val pairOfCorrectPositions: Pair<Int, Int>
 ){
     data class ItemBoardEntity(
         val imageUrl: String,
--- feature/trvia/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/memorize_game/MemorizeGameUIState.kt
+++ feature/trvia/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/memorize_game/MemorizeGameUIState.kt
@@ -1,18 +1,23 @@
 package com.chocolatecake.viewmodel.memorize_game
 
 data class MemorizeGameUIState(
+    val initialCounter: Int = 30,
     val countDownTimer: Int = 30,
     val level: Int = 1,
     val board: List<ItemGameImageUiState> = emptyList(),
     val initialBoard: List<ItemGameImageUiState> = emptyList(),
+    val selectedUserPositions: List<Int> = emptyList(),
     val boardSize: Int = 12,
     val points: Int = 0,
-    val CorrectPairPositions: Pair<Int,Int> = Pair(0,1),
     val firstUserPosition: Int? = null,
     val secondUserPosition: Int? = null,
     val isLoading: Boolean = true,
     val isError: Boolean = false
-)
+){
+    val isWinner: Boolean
+        get() = boardSize == selectedUserPositions.size
+
+}
 
 data class ItemGameImageUiState(
     val imageUrl: String = "",
