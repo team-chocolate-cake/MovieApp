12a8c4fbfb95cc838809be61969ffa7ab8f8c608
Mahmoud <Elboray>
2023-06-27 01:17:52 +0300

    add useCases to Reset data level

---
+++ feature/trvia/domain/usecase/src/main/java/com/chocolatecake/usecase/game/levelup/ResetLevelUpTvShowUseCase.kt
@@ -0,0 +1,21 @@
+package com.chocolatecake.usecase.game.levelup
+
+import com.chocolatecake.repository.TriviaRepository
+import com.chocolatecake.usecase.GetCurrentUserUseCase
+import javax.inject.Inject
+
+class ResetLevelUpTvShowUseCase @Inject constructor(
+    private val triviaRepository: TriviaRepository,
+    private val getCurrentUserUseCase: GetCurrentUserUseCase
+) {
+    suspend operator fun invoke(): Boolean {
+        val oldUser = getCurrentUserUseCase()
+        if (oldUser.tvShowGameLevel == 1 && oldUser.numMoviesQuestionsPassed == 0) return false
+        val updatedUser = oldUser.copy(
+            tvShowGameLevel = 1,
+            numMoviesQuestionsPassed = 0
+        )
+        triviaRepository.updateUser(updatedUser)
+        return true
+    }
+}
