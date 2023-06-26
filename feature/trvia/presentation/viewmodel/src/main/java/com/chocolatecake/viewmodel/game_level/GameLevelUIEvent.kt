49b2116db0722021ed639ada0a8ad4acf1f88378
Mahmoud <Elboray>
2023-06-27 01:22:38 +0300

    add two object ResetLevels and BackNavigate

--- feature/trvia/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/game_level/GameLevelUIEvent.kt
+++ feature/trvia/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/game_level/GameLevelUIEvent.kt
@@ -1,10 +1,14 @@
 package com.chocolatecake.viewmodel.game_level
 
+import com.chocolatecake.viewmodel.game_type.GameTypeUIEvent
+
 sealed interface GameLevelUIEvent {
     object NavigateToPeopleGame : GameLevelUIEvent
     object NavigateToMovieGame : GameLevelUIEvent
     object NavigateToTvGame : GameLevelUIEvent
     object NavigateToMemorizeBoard : GameLevelUIEvent
     object ShowYouMustPassPreviousLevelFirst : GameLevelUIEvent
+    object ResetLevels : GameLevelUIEvent
+    object BackNavigate : GameLevelUIEvent
     data class ShowSnckbar(val message: String) : GameLevelUIEvent
 }
