2bf1d802efcc0afde8f5cd6a2192c92ee15d142e
Mahmoud <Elboray>
2023-06-27 01:32:20 +0300

    add fun play sound to turn on when i pressed on button

--- feature/trvia/presentation/ui/src/main/java/com/chocolatecake/ui/home/HomeGameFragment.kt
+++ feature/trvia/presentation/ui/src/main/java/com/chocolatecake/ui/home/HomeGameFragment.kt
@@ -7,27 +7,37 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.chocolatecake.ui.sound_when_play.SoundManager
 import com.chocolatecake.ui.trivia.R
 import com.chocolatecake.ui.trivia.databinding.FragmentGameHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class HomeGameFragment : Fragment() {
     lateinit var binding: FragmentGameHomeBinding
+    @Inject
+    lateinit var soundManager: SoundManager
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_game_home, container, false)
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         binding.buttonStart.setOnClickListener {
             findNavController().navigate(HomeGameFragmentDirections.actionHomeGameFragmentToTypeGameFragment())
+            soundManager.playSound(R.raw.sound)
         }
+
+
     }
 }
