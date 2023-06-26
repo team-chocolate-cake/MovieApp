f6338fc27aba7d3be88b82b31bc9b9744065ed70
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-06-26 15:23:14 +0300

    fix: season details visibility

--- feature/home/presentation/home-ui/src/main/java/com/chocolatecake/ui/season_details/SeasonDetailsItem.kt
+++ feature/home/presentation/home-ui/src/main/java/com/chocolatecake/ui/season_details/SeasonDetailsItem.kt
@@ -1,10 +1,9 @@
 package com.chocolatecake.ui.season_details
 
 import com.chocolatecake.viewmodel.common.model.EpisodeHorizontalUIState
-import com.chocolatecake.viewmodel.season_details.SeasonDetailsUiState
 
 sealed class SeasonDetailsItem(val type: SeasonDetailsType){
-    data class OverviewItem(val overview: String):
+    data class OverviewItem(val overview: String, val isEmptyEpisodes: Boolean):
         SeasonDetailsItem(SeasonDetailsType.OVERVIEW)
 
     data class EpisodeItem(val episodeHorizontalUIState: EpisodeHorizontalUIState):
