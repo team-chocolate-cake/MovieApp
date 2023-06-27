23ba0beb00fc81b9c70d05844ef4ec106354fdf9
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-06-27 22:36:44 +0300

    solve conflict

--- feature/trvia/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/tv_show_guessing/TvShowGuessingViewModel.kt
+++ feature/trvia/presentation/viewmodel/src/main/java/com/chocolatecake/viewmodel/tv_show_guessing/TvShowGuessingViewModel.kt
@@ -4,11 +4,10 @@ import com.chocolatecake.entities.QuestionEntity
 import com.chocolatecake.entities.UserEntity
 import com.chocolatecake.usecase.GetCurrentUserUseCase
 import com.chocolatecake.usecase.game.UpdateUserPointsUseCase
-import com.chocolatecake.usecase.game.levelup.LevelUpMoviesUseCase
 import com.chocolatecake.usecase.game.levelup.LevelUpTvShowUseCase
 import com.chocolatecake.usecase.game.questions.GetCurrentTvShowQuestion
 import com.chocolatecake.usecase.game.questions.UpdateTvShowsQuestionCountUseCase
-import com.chocolatecake.viewmodel.common.BaseTriviaQuestionGameViewModel
+import com.chocolatecake.viewmodel.common.BaseTriviaAnswerGameViewModel
 import com.chocolatecake.viewmodel.common.model.GameType
 import com.chocolatecake.viewmodel.common.model.GameUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -22,7 +21,7 @@ class TvShowGuessingViewModel @Inject constructor(
     private val updateTvShowsQuestionCountUseCase: UpdateTvShowsQuestionCountUseCase,
     override val updateUserPointsUseCase: UpdateUserPointsUseCase,
     private val levelUpTvShowUseCase: LevelUpTvShowUseCase,
-):BaseTriviaQuestionGameViewModel(state = GameUiState()) {
+):BaseTriviaAnswerGameViewModel(state = GameUiState()) {
     override val gameType: GameType = GameType.TV_SHOW
 
     init {
